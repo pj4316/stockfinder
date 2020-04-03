c8e0ddcbbf43954090a9af804285fb52acae148f
maxlee <pj4316@naver.com>
2020-04-03 23:39:33 +0900

    init

---
+++ src/test/java/com/maxlee/stockfinder/StockfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxlee.stockfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
