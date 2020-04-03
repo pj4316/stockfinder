51f2e48137573a6bc8f2d11cd456b1806a379fe4
maxlee <pj4316@naver.com>
2020-04-04 00:03:09 +0900

    getStockInfoOfDate controller 뼈대 구성

--- src/main/java/com/maxlee/stockfinder/controller/StockController.java
+++ src/main/java/com/maxlee/stockfinder/controller/StockController.java
@@ -1,6 +1,8 @@
 package com.maxlee.stockfinder.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class StockController {
     @GetMapping("test")
     public String test() {
         return "test";
+    }
+
+    @PostMapping("getStockInfoOfDate")
+    public String getStockInfoOfDate(@RequestParam("stockItem") String stockItem) {
+        // TODO : naver finance api를 통한 특정 회사의 일별 주가 데이터 얻어오기
+        return stockItem;
     }
 }
